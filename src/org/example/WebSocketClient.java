3d3aaa8496fcb2f7c31f6fde1cbd275892be7b8d
kalingah <kalingah@gmail.com>
2024-09-27 16:03:34 +0530

    Update WebSocketClient.java
    added one more comment to see the webhook payload

--- src/org/example/WebSocketClient.java
+++ src/org/example/WebSocketClient.java
@@ -14,6 +14,7 @@ import javax.annotation.PostConstruct;
 public class WebSocketClient extends TextWebSocketHandler {
 
     //added this comment and edited
+    //added one more comment
     @PostConstruct
     public void connect() throws Exception {
         // Create the WebSocket client
